ff7aa66b78ff119fd281eb5a6b8a76a5b8488908
Vladimir Rybalsky <alevoldon@gmail.com>
2020-08-06 12:12:24 +0300

    add files

---
+++ 02. SYNTAX PHP. INCLUDING PHP SCENARIOS/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>02. SYNTAX PHP. INCLUDING PHP SCENARIOS</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<?php 
+			echo "<h1>Курс PHP для <i>CodiFun</i></h1>";
+		 ?>
+	</div>
+</body>
+</html>
---
+++ 03. INSTRUCTIONS FOR DISPLAYING DATA IN PHP. VARIABLES IN PHP/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>03. INSTRUCTIONS FOR DISPLAYING DATA IN PHP. VARIABLES IN PHP</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+<?php 
+	echo "<h1>How to output text...</h1>";
+	print "<p>...with 'echo' or 'print'.</p>";
+ ?>
+
+ <?php 
+ 	$name = "Maria";
+ 	$ages = 27;
+ 	$weight = 67.8;
+ 	echo $name;
+ 	echo "<br/>";
+ 	echo $ages;
+ 	echo "<br/>";
+ 	echo $weight;
+  ?>
+
+  <br>
+  <br>
+  <br>
+
+  <?php
+   /*Начинаю комментарий на несколько строк и ниже я определю несколько переменных */ 
+  	$name = "Maria";
+  	$ages = 27;
+  	$weight = 67.8;
+  	echo $name;
+  	echo "<br/>";
+  	# я включил комментарий на одной строке здесь
+  	echo "$ages лет";
+  	echo "<br/>";
+  	// и тут я включил комментарий на одной строке
+  	echo 'и имеет вес $weight';
+   ?>
