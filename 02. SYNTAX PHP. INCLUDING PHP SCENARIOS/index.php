395354ffd706ddf9a84a3335362e853e1abfac1c
Vladimir Rybalsky <alevoldon@gmail.com>
2020-08-06 15:14:41 +0300

    update files

--- 02. SYNTAX PHP. INCLUDING PHP SCENARIOS/index.php
+++ 02. SYNTAX PHP. INCLUDING PHP SCENARIOS/index.php
@@ -2,8 +2,18 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>02. SYNTAX PHP. INCLUDING PHP SCENARIOS</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>02.SYNTAX PHP. INCLUDING PHP SCENARIOS</title>
+	<style type="text/css">
+		body {
+			font-family: Arial, Helvetica, sans-serif;
+		}
+
+		h1 { 
+			background: rgb(93,191,172);
+			color: #fff;
+			text-align: center;
+			}
+	</style>
 </head>
 <body>
 	<div class="container">
--- 03. INSTRUCTIONS FOR DISPLAYING DATA IN PHP. VARIABLES IN PHP/index.php
+++ 03. INSTRUCTIONS FOR DISPLAYING DATA IN PHP. VARIABLES IN PHP/index.php
@@ -2,28 +2,33 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>03. INSTRUCTIONS FOR DISPLAYING DATA IN PHP. VARIABLES IN PHP</title>
+	<title>03.INSTRUCTIONS FOR DISPLAYING DATA IN PHP. VARIABLES IN PHP</title>
+	<style type="text/css">
+		body {
+			font-family: Arial, Helvetica, sans-serif;
+		}
+	</style>
 </head>
 <body>
 	<?php 
-	echo "<h1>How to output text...</h1>";
-	print "<p>...with 'echo' or 'print'.</p>";
- ?>
+		echo "<h1>How to output text...</h1>";
+		print "<p>...with 'echo' or 'print'.</p>";
+ 	?>
 
- <?php 
- 	$name = "Maria";
- 	$ages = 27;
- 	$weight = 67.8;
- 	echo $name;
- 	echo "<br/>";
- 	echo $ages;
- 	echo "<br/>";
- 	echo $weight;
-  ?>
+ 	<?php 
+ 		$name = "Maria";
+ 		$ages = 27;
+ 		$weight = 67.8;
+ 		echo $name;
+ 		echo "<br/>";
+ 		echo $ages;
+ 		echo "<br/>";
+ 		echo $weight;
+  	?>
 
-  <br>
-  <br>
-  <br>
+  		<br>
+  		<br>
+  		<br>
 
   <?php
    /*Начинаю комментарий на несколько строк и ниже я определю несколько переменных */ 
